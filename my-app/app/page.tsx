1138a57205d1c1d37123e50dfa41a8b13450e6ad
Jose Plaud <joseenrique1031@gmail.com>
2024-04-09 19:37:22 -0400

    Frontend Components

---
+++ my-app/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Dashboard } from "@/components/authentication";
+import { TextareaWithButton } from "@/components/submit";
+
+export default function Home() {
+  return (
+    <Dashboard/>
+  );
+}
