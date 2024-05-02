582b36ff0f862e4b7283d35a7348ce18a6ed1813
Jose Plaud <joseenrique1031@gmail.com>
2024-05-02 11:54:23 -0400

    structure

--- my-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import { Dashboard } from "@/components/authentication";
-import { TextareaWithButton } from "@/components/submit";
-
-export default function Home() {
-  return (
-    <Dashboard/>
-  );
-}
